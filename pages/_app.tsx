eb4df57cca5f8acce92e804c9596265c9b6a56f4
BRIAN <107471917+sxadxsx@users.noreply.github.com>
2023-12-13 22:42:20 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,27 @@
 import '@/styles/globals.css'
+import "nprogress/nprogress.css";
+import { useRouter } from "next/router";
+import NProgress from "nprogress";
+import { useEffect } from "react";
 import type { AppProps } from 'next/app'
 import Head from './components/head'
 import Footer from './components/footer'
 import { SessionProvider } from 'next-auth/react'
 import Script from 'next/script';
-
+NProgress.configure({ showSpinner: false });
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
+   const router = useRouter();
+
+  // Integrate nprogress
+  useEffect(() => {
+    router.events.on('routeChangeStart', () => NProgress.start());
+    router.events.on('routeChangeComplete', () => NProgress.done());
+    router.events.on('routeChangeError', () => NProgress.done());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   // 检查是否是 RestrictedPage，如果是，则不使用 app.tsx
   if (Component.name === 'support') {
     return (
