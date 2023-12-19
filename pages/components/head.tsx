5fcbd640dc2c4ed6f9aee35831659f048f5ca648
BRIAN <107471917+sxadxsx@users.noreply.github.com>
2023-12-19 22:20:16 +0800

    Update head.tsx

--- pages/components/head.tsx
+++ pages/components/head.tsx
@@ -196,6 +196,7 @@ export default function Head() {
               indexName="help"
             />
                               </div>
+                            <div className="py-6">
                 <a
                   href="#"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
