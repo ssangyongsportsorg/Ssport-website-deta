e5b2cdda6f27b3015ca09ccbfd00dd728aac8355
BRIAN <107471917+sxadxsx@users.noreply.github.com>
2023-12-19 18:11:45 +0800

    Update head.tsx

--- pages/components/head.tsx
+++ pages/components/head.tsx
@@ -29,6 +29,7 @@ const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
           fluid={true}
           rounded={true}
           className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600"
+          open={mobileMenuOpen} onClose={setMobileMenuOpen}
         >
           <Link href="/">
             <img
