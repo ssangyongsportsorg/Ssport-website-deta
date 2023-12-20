97ea32f366b1a4da378627ff624079fb5fe72b16
BRIAN <107471917+sxadxsx@users.noreply.github.com>
2023-12-20 21:47:51 +0800

    Update head.tsx

--- pages/components/head.tsx
+++ pages/components/head.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import { DocSearch } from '@docsearch/react'
 import a from './docsearch.module.css';
 import '@docsearch/css';
-
+import Login from "./login";
 import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react'
 import {
   ArrowPathIcon,
@@ -52,7 +52,7 @@ export default function Head() {
         <Popover.Group className="hidden lg:flex lg:gap-x-12">
           <Popover className="relative">
             <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
-              Product
+              體育
               <ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
             </Popover.Button>
 
@@ -122,14 +122,12 @@ export default function Head() {
           
         </Popover.Group>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <DocSearch
+                   <DocSearch
               apiKey="c2e792c2e75fe1dd3e40574f8b4c9a80"
               appId="70GEOCJCSX"
               indexName="help"
             />
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Log in <span aria-hidden="true">&rarr;</span>
-          </a>
+          <Login />
         </div>
       </nav>
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
@@ -137,10 +135,10 @@ export default function Head() {
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
+              <span className="sr-only">雙龍體育</span>
               <img
                 className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                src="/logo.png"
                 alt=""
               />
             </a>
@@ -160,7 +158,7 @@ export default function Head() {
                   {({ open }) => (
                     <>
                       <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                        Product
+                        體育
                         <ChevronDownIcon
                           className={classNames(open ? 'rotate-180' : '', 'h-5 w-5 flex-none')}
                           aria-hidden="true"
@@ -223,14 +221,9 @@ export default function Head() {
               appId="70GEOCJCSX"
               indexName="help"
             />
-                              </div>
-                            <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Log in
-                </a>
+                             
+                        <Login />
+
               </div>
             </div>
           </div>
