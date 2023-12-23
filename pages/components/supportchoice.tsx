9ce6230b789ced806a5a75bbfb0ee260fbe83e0b
BRIAN <107471917+sxadxsx@users.noreply.github.com>
2023-12-24 00:11:31 +0800

    Update supportchoice.tsx

--- pages/components/supportchoice.tsx
+++ pages/components/supportchoice.tsx
@@ -5,7 +5,7 @@ function supportchoice() {
   <div className="max-w-screen-xl px-4 py-8 mx-auto lg:py-16">
     <div className="space-y-4 sm:grid sm:grid-cols-2 lg:grid-cols-4 sm:gap-4 xl:gap-8 sm:space-y-0 md:mt-12">
       <a
-        href="https://help.ssangyongsports.org/"
+        href="#"
         className="block px-8 py-12 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light"
       >
         <span
@@ -27,17 +27,17 @@ function supportchoice() {
           <img
             alt=""
             className="service-icon"
-            src="https://th.bing.com/th/id/OIG.416Ubl3HuCXju7xRYO4m?w=173&h=173&c=6&r=0&o=5&dpr=1.3&pid=ImgGn"
+            src="https://ssport-img.vercel.app/come.jpg"
             width={75}
           />
           <noscript />
         </span>
         <h3 className="font-semibold text-xl text-gray-900 dark:text-white mt-3.5">
-          幫助中心
+          正在撰寫
         </h3>
       </a>
       <a
-        href="https://ssangyongsports.org/c"
+        href="#"
         className="block px-8 py-12 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light"
       >
         <span
@@ -59,17 +59,17 @@ function supportchoice() {
           <img
             alt=""
             className="service-icon"
-            src="https://th.bing.com/th/id/OIG.C96gOk0chUn1kKHxKOg3?w=173&h=173&c=6&r=0&o=5&dpr=1.3&pid=ImgGn"
+            src="https://ssport-img.vercel.app/come.jpg"
             width={75}
           />
           <noscript />
         </span>
         <h3 className="font-semibold text-xl text-gray-900 dark:text-white mt-3.5">
-          聯繫我們
+          正在撰寫
         </h3>
       </a>
       <a
-        href="https://discuss.ssangyongsports.org/"
+        href="/docs/contact"
         className="block px-8 py-12 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light"
       >
         <span
@@ -91,17 +91,17 @@ function supportchoice() {
           <img
             alt=""
             className="service-icon"
-            src="https://th.bing.com/th/id/OIG.cyGRgj1FvZpQO.xkVoOi?w=173&h=173&c=6&r=0&o=5&dpr=1.3&pid=ImgGn"
+            src="https://ssport-img.vercel.app/contact.JPG"
             width={75}
           />
           <noscript />
         </span>
         <h3 className="font-semibold text-xl text-gray-900 dark:text-white mt-3.5">
-          論壇
+          聯繫
         </h3>
       </a>
       <a
-        href="https://status.ssangyongsports.org/"
+        href="#"
         className="block px-8 py-12 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light"
       >
         <span
@@ -123,13 +123,13 @@ function supportchoice() {
           <img
             alt=""
             className="service-icon"
-            src="https://th.bing.com/th/id/OIG.OaAnbr7kjUA3SffiMoCl?w=173&h=173&c=6&r=0&o=5&dpr=1.3&pid=ImgGn"
+            src="https://ssport-img.vercel.app/come.jpg"
             width={75}
           />
           <noscript />
         </span>
         <h3 className="font-semibold text-xl text-gray-900 dark:text-white mt-3.5">
-          狀態
+          正在撰寫
         </h3>
       </a>
     </div>
