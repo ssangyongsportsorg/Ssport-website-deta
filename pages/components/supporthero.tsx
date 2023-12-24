9d507d2fab9d8596f0f57bb8c9942c5ba3577f5b
BRIAN <107471917+sxadxsx@users.noreply.github.com>
2023-12-24 21:50:35 +0800

    Update supporthero.tsx

--- pages/components/supporthero.tsx
+++ pages/components/supporthero.tsx
@@ -10,8 +10,8 @@ function supporthero() {
   className="bg-gradient-to-r from-purple-600 via-blue-500 to-indigo-900"
 >
   <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
-    <h2 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-      需要 <span className={styles.abc}>雙龍體育</span>的幫助嗎?
+    <h2 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
+      需要 <span className={styles.c}>雙龍體育</span>的幫助嗎?
     </h2>
         <div className={`${a.docSearch} ${a.container}`}>
     <DocSearch
