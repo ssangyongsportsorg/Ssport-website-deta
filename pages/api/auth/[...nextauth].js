9274c900a1dd43683ba41014b87ff7df185e157d
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-12-30 14:38:20 +0800

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,31 +1,41 @@
-import NextAuth from 'next-auth';
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import LineProvider from "next-auth/providers/line";
+import DiscordProvider from "next-auth/providers/discord";
 
-const options = {
+export const authOptions = {
   providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
     GoogleProvider({
       clientId: process.env.Google_ID,
       clientSecret: process.env.Google_SECRET,
     }),
-    // 其他驗證提供者設置
+    LineProvider({
+      clientId: process.env.Line_ID,
+      clientSecret: process.env.Line_SECRET,
+    }),
+    DiscordProvider({
+      clientId: process.env.Discord_ID,
+      clientSecret: process.env.Discord_SECRET,
+    }),
   ],
-  theme: {
-    logo: "/logo.png", // Absolute URL to image
-  },
   callbacks: {
-    async session(session, user) {
-      session.user = user;
-      return session;
-    },
-    async jwt(token, user, account, profile, isNewUser) {
-      if (user) {
-        token.role = user.role; // 將使用者的角色存儲在 JWT token 中
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
       }
       return token;
     },
+    async session({ session, token, user }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
     async signIn(user) {
-      // 檢查使用者的電子郵件地址
-      if (user.email === 'didk@dod.com') {
+      if (user.email === 'ksks@gmail.com') {
         user.role = 'admin'; // 將使用者角色設置為 "admin"
       }
       return true;
@@ -33,4 +43,4 @@ const options = {
   },
 };
 
-export default (req, res) => NextAuth(req, res, options);
+export default NextAuth(authOptions);
