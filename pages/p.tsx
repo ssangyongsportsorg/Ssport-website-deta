1c84ee9819c9a5c3f4835e72cc9ad561ce071942
BRIAN <107471917+sxadxsx@users.noreply.github.com>
2023-12-31 12:29:24 +0800

    Update p.tsx

--- pages/p.tsx
+++ pages/p.tsx
@@ -1,8 +1,11 @@
 import Head from 'next/head'
+import Scroll from '../components/scroll';
+
 const p = () => {
   return (
 
 <main className="pt-8 pb-16 lg:pt-16 lg:pb-24">
+     <Scroll />
   <div className="flex justify-between px-4 mx-auto max-w-screen-xl ">
     <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
         <header className="mb-4 lg:mb-6 not-format">        
