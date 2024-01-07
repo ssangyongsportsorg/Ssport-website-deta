e55781c5f49a3bb6d0f7ef87aa1ef86643306378
BRIAN <107471917+sxadxsx@users.noreply.github.com>
2024-01-07 22:05:03 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,3 @@
-import { RootProvider } from 'next-docs-ui/provider'
-import { Inter } from 'next/font/google'
-import type { ReactNode } from 'react'
-import 'next-docs-ui/style.css'
-import '@/styles/globals.css'
-import Footer from '@/pages/components/footer'
 
 
 
@@ -20,5 +14,41 @@ export default function Layout({ children }: { children: ReactNode }) {
     <Footer />   
     </html>
     
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+import dynamic from 'next/dynamic'
+import { RootProvider } from 'next-docs-ui/provider'
+import { Inter } from 'next/font/google'
+import type { ReactNode } from 'react'
+import 'next-docs-ui/style.css'
+import '@/styles/globals.css'
+import Footer from '@/pages/components/footer'
+
+export default function RootLayout({ children }) {
+  const CrispWithNoSSR = dynamic(
+    () => import('../components/crisp')
+  )
+
+  return (
+    <html lang="zh-tw">
+    <CrispWithNoSSR />
+    <body>
+<RootProvider>        {children}</RootProvider>
+    </body>
+          <Footer />   
+
+    </html>
   )
 }
