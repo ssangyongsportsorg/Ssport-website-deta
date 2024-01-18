cee4aa9780ea8fdaabef704daeabbbe1c04cc17b
BRIAN <107471917+sxadxsx@users.noreply.github.com>
2024-01-18 21:48:57 +0800

    Update head.tsx

--- pages/components/head.tsx
+++ pages/components/head.tsx
@@ -196,7 +196,7 @@ export default function Head() {
                   隱私政策
                 </Link>
                 <Link
-                  href="/p"
+                  href="/t"
                   onClick={() => setMobileMenuOpen(false)}
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
