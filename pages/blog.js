f3009e15e0f178dec51c7388ea178f9973762006
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-01-20 18:47:16 +0800

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -11,21 +11,22 @@ export default function Blog({posts}){
     return <main>
         <div>
         <section>
-  <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-    <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
-      <h2 className={`mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white `}>
-        雙龍體育
-            <span className={styles.c}>部落格</span>
+  <div className="bg-white py-24 sm:py-32">
+  <div className="mx-auto max-w-7xl px-6 lg:px-8">
+    <div className="mx-auto max-w-2xl lg:mx-0">
+      <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+        From the blog
       </h2>
-      <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">
-   任何關於雙龍體育的任何關於雙龍體育的消息和公告
+      <p className="mt-2 text-lg leading-8 text-gray-600">
+        Learn how to grow your business with our expert advice.
       </p>
-      <Head>
+    </div>
+  </div>
+</div>
+<Head>
         <title>雙龍體育blog</title>
         <meta name="description" content="任何關於雙龍體育的任何關於雙龍體育的消息和公告,就上雙龍體育blog" />
       </Head>
-    </div>
-  </div>
 </section>
         </div>
                 
@@ -39,65 +40,47 @@ export default function Blog({posts}){
             const {title, seo, author, category, date, bannerImage, tags, img, info} = frontmatter
 
             //JSX for individual blog listing
-            return <article key={title}>
-            
-         <div className="flex justify-between px-4 mx-auto max-w-8xl">
-  <div className="hidden mb-6 xl:block lg:w-80">
-    <div className="sticky top-36">
+            return <article className="flex max-w-xl flex-col items-start justify-between" key={title}>
+  <div className="flex items-center gap-x-4 text-xs">
+    <time dateTime="{date}" className="text-gray-500">
+      {date}
+    </time>
+    <a
+      href="#"
+      className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+    >
+         {tags}
+    </a>
+  </div>
+  <div className="group relative">
+    <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+      <Link href={`/blog/${slug}`}>
+        <span className="absolute inset-0" />
+                    {title}
+      </Link>
+    </h3>
+    <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+      {seo}
+    </p>
+  </div>
+  <div className="relative mt-8 flex items-center gap-x-4">
+    <Image
+      src={img}
+      alt={author}
+      className="h-10 w-10 rounded-full bg-gray-50"
+    />
+    <div className="text-sm leading-6">
+      <p className="font-semibold text-gray-900">
+        <a href="#">
+          <span className="absolute inset-0" />
+           {author}
+        </a>
+      </p>
+      <p className="text-gray-600"> {info}</p>
     </div>
   </div>
-  <div className="w-full max-w-2xl mx-auto">
-    <div className="divide-y divide-gray-200 dark:divide-gray-700">
-      <article className="py-6">
-        <div className="flex items-center justify-between mb-3 text-gray-500">
-          <div>
-            <Link
-              className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 hover:bg-blue-200 dark:hover:bg-blue-300 dark:text-blue-800 mb-2"
-              href="#"
-            >
-              #{tags}
-            </Link>
-          </div>
-          <span className="text-sm">
-            {date}
-          </span>
-        </div>
-    <h2 className={`mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white `}>
-    <Link href={`/blog/${slug}`}>
-            {title}
-          </Link>
-        </h2>
-        <p className="mb-5 text-gray-500 dark:text-gray-400">
-          {seo}
-        </p>
-        <div className="flex items-center justify-between">
-          <a
-            className="flex items-center space-x-2"
-            href="https://discuss.ssangyongsports.org/members/peter-yang.1/"
-          >
-            <Image
-  className="rounded-full w-7 h-7"
-  src={img}
-  alt={author}
-/>
-            
-            <span className="font-medium dark:text-white">
-               {author}
-            </span>
-          </a>
-          <Link
-            href={`/blog/${slug}`}
-          >
-                    閱讀文章
-          
-          </Link>
-        </div>
-      </article>
-    </div>
-  </div>
-</div>
-
 </article>
+
 
         })}
 </main>
