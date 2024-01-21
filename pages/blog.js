5aa1bd048898cdb2bbf6fd310ab84a113c35255f
BRIAN <107471917+sxadxsx@users.noreply.github.com>
2024-01-21 21:54:51 +0800

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -21,8 +21,131 @@ export default function Blog({posts}){
         這裡找到最新公告、部落格文章
       </p>
     </div>
+    <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+      <article className="flex max-w-xl flex-col items-start justify-between">
+        <div className="flex items-center gap-x-4 text-xs">
+          <time dateTime="2023-12-21" className="text-gray-500">
+            2023-12-21
+          </time>
+          <a
+            href="#"
+            className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+          >
+            公告
+          </a>
+        </div>
+        <div className="group relative">
+          <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+            <Link href="/blog/close">
+              <span className="absolute inset-0" />
+              雙龍體育暫停服務通知：我們的補償方案和未來展望
+            </Link>
+          </h3>
+          <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+          比賽將在2024 6月重新開打，We will come back soon
+          </p>
+        </div>
+        <div className="relative mt-8 flex items-center gap-x-4">
+          <img
+            src="https://ssangyongsports.eu.org/team.jpeg"
+            alt=""
+            className="h-10 w-10 rounded-full bg-gray-50"
+          />
+          <div className="text-sm leading-6">
+            <p className="font-semibold text-gray-900">
+              <a href="#">
+                <span className="absolute inset-0" />
+                雙龍體育
+              </a>
+            </p>
+            <p className="text-gray-600">雙龍體育官方行政團隊</p>
+          </div>
+        </div>
+      </article>
+      <article className="flex max-w-xl flex-col items-start justify-between">
+        <div className="flex items-center gap-x-4 text-xs">
+          <time dateTime="2023-08-2" className="text-gray-500">
+           2023-08-2
+          </time>
+          <a
+            href="#"
+            className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+          >
+            公告
+          </a>
+        </div>
+        <div className="group relative">
+          <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+            <Link href="/blog/logo">
+              <span className="absolute inset-0" />
+              公告: 發表全新Logo
+            </Link>
+          </h3>
+          <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+            新logo發佈通知
+          </p>
+        </div>
+        <div className="relative mt-8 flex items-center gap-x-4">
+          <img
+            src="https://ssangyongsports.eu.org/team.jpeg"
+            alt=""
+            className="h-10 w-10 rounded-full bg-gray-50"
+          />
+          <div className="text-sm leading-6">
+            <p className="font-semibold text-gray-900">
+              <a href="#">
+                <span className="absolute inset-0" />
+                雙龍體育
+              </a>
+            </p>
+            <p className="text-gray-600">雙龍體育團隊</p>
+          </div>
+        </div>
+      </article>
+      <article className="flex max-w-xl flex-col items-start justify-between">
+        <div className="flex items-center gap-x-4 text-xs">
+          <time dateTime="2024-01-7" className="text-gray-500">
+            2024-01-7
+          </time>
+          <a
+            href="#"
+            className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+          >
+            blog
+          </a>
+        </div>
+        <div className="group relative">
+          <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+            <Link href="/blog/dub">
+              <span className="absolute inset-0" />
+              如何使用 dub.sh 創建簡短的網址
+            </Link>
+          </h3>
+          <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+           使用dub創建短網址
+          </p>
+        </div>
+        <div className="relative mt-8 flex items-center gap-x-4">
+          <img
+            src="https://ssangyongsports.eu.org/1D7F9E91-14B0-4914-9FBD-0E46CE340A78.png"
+            alt=""
+            className="h-10 w-10 rounded-full bg-gray-50"
+          />
+          <div className="text-sm leading-6">
+            <p className="font-semibold text-gray-900">
+              <a href="#">
+                <span className="absolute inset-0" />
+               Peter yang
+              </a>
+            </p>
+            <p className="text-gray-600">雙龍體育CEO</p>
+          </div>
+        </div>
+      </article>
+    </div>
   </div>
 </div>
+
 <Head>
         <title>雙龍體育blog</title>
         <meta name="description" content="任何關於雙龍體育的任何關於雙龍體育的消息和公告,就上雙龍體育blog" />
