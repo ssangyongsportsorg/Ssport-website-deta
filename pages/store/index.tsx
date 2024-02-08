e7214f2c2640f27d8d43ab48130c529315bf4701
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-02-08 11:52:31 +0800

    Update index.tsx

--- pages/store/index.tsx
+++ pages/store/index.tsx
@@ -13,7 +13,7 @@ const Home = () => {
 
       <div>
         <div className="bg-gradient h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
-        <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div>
           <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
             <div
               className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
