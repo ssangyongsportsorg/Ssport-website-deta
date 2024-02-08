4fa4125686efc43a00737694effa56ada1ea5190
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-02-08 12:09:54 +0800

    Update index.tsx

--- pages/store/index.tsx
+++ pages/store/index.tsx
@@ -66,7 +66,7 @@ const Home = () => {
       <div className="group relative">
         <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
           <img
-            src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
+            src="https://i.imgur.com/SMZkCQm.png"
             alt="Front of men's Basic Tee in black."
             className="h-full w-full object-cover object-center lg:h-full lg:w-full"
           />
