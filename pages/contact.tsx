afe6e08c795848793e23ea34c13b5e6b33a62799
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-02-17 19:56:10 +0800

    Update contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -41,6 +41,7 @@ function C() {
   你的電子郵件
 
 <input
+  id="Email"
   type="name"
   name="email"
   className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
