385eb1705fb1ae7a1d48757caff3ee47b4ad5632
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-02-17 20:00:16 +0800

    Update contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -42,8 +42,8 @@ function C() {
 
 <input
   id="Email"
-  type="name"
-  name="email"
+  type="email"
+  name="Email"
   className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
   placeholder="peter@ssangyongsports.org"
   required={true}
@@ -102,11 +102,7 @@ function C() {
           defaultValue={""}
         />
           </label>
-<input
-  type="hidden"
-  name="_cc"
-  defaultValue="support@ssangyongsports.eu.org"
-/>
+
 
       <button
         type="submit"
