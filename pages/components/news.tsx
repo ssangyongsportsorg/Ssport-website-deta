edc541aeca3024c235e00e3e95721ca7411e2154
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-02-22 21:51:12 +0800

    Update news.tsx

--- pages/components/news.tsx
+++ pages/components/news.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 function news() {
     return (
 <section className="bg-white dark:bg-gray-900">
-  <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+  <div className="py-8 px-4 mx-auto  lg:py-16 lg:px-6">
     <div className="mx-auto max-w-screen-md sm:text-center">
       <h2 className="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl dark:text-white">
         訂閱最新消息
