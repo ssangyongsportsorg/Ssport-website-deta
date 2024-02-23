3291f497a5c7ea9bbbbe9241b17bb5b5246e0e0f
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-02-23 21:43:49 +0800

    Update action.tsx

--- pages/components/action.tsx
+++ pages/components/action.tsx
@@ -9,7 +9,7 @@ import Link from "next/link"
 
 export default function Component() {
   return (
-    <section className="w-full h-screen py-6 md:py-12 lg:py-20 xl:py-24 bg-black">
+    <section className="h-screen py-6 md:py-12 lg:py-20 xl:py-24 bg-black">
   <div className="container px-4 md:px-6">
     <div className="grid gap-6 items-center">
       <div className="flex flex-col justify-center space-y-4 text-center">
