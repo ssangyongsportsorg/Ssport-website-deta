f502df61fd573f1b0394eace8cb301db11521b54
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-02-28 10:23:57 +0800

    Update contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -72,14 +72,14 @@ function C() {
       </Select>
     </div>
         <label
-          htmlFor="subject"
+          htmlFor="Subject"
           className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
         >
           主旨
         
         <input
           type="text"
-  name="_subject"
+  name="Subject"
     defaultValue={subjectDefaultValue} // 在此處設置預設值
           required={true}
           className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
