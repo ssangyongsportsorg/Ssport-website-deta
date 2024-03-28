95bc4a63efbd71eee9ef5de9287be2063a25c718
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-03-28 21:40:32 +0800

    Update contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -15,6 +15,7 @@ function C() {
    if (mode === 'status') {
     subjectDefaultValue = '報告錯誤(不要更改此處,有助於加速回復)';
   }
+
   return (
         <div>
    <section>
@@ -97,7 +98,6 @@ function C() {
           name="message"
           rows={6}
           required={true}
-         defaultValue={input} // 在此處設置預設值
           className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
           placeholder="Leave a comment..."
           defaultValue={""}
