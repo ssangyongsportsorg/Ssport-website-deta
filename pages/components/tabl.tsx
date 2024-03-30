c5306410dfbfdafbf85aff39f537580191073324
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-03-30 18:13:33 +0800

    Update tabl.tsx

--- pages/components/tabl.tsx
+++ pages/components/tabl.tsx
@@ -4,7 +4,7 @@ import styles from './Button.module.css';
 
 function Tabl() {
   return (
-    <section style={{ backgroundColor: 'blue' }}>
+    <section style={{ backgroundColor: '#ffefd5' }}>
       <Image
           className="w-full"
           src="https://static.remove.bg/seperator-start.svg"
