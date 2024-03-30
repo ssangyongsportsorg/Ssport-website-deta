6bf6b34982e6902522d1265405d080c6918d0961
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-03-30 17:45:34 +0800

    Update tabl.tsx

--- pages/components/tabl.tsx
+++ pages/components/tabl.tsx
@@ -15,7 +15,7 @@ function Tabl() {
           雙龍職籃是雙龍體育旗下的體育組織,總共有四支隊伍,第一屆冠軍由steven帶領拿下冠軍
           </p>
           <a
-            href="https://sba.ssangyongsports.eu.org/"
+            href="https://sba.ssangyongsports.org/"
             className="inline-flex items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900"
           >
            更多資訊
