c6186954ffda9c21500eec3e2c4f87581b976d4c
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-03-31 23:38:50 +0800

    Update pages

---
+++ pages/feedback/status.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import styles from './components/Button.module.css';
+function status() {
+  return (   
+        <div>
+ <section className="bg-white dark:bg-gray-900">
+  <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+<iframe src="https://feedback.ssangyongsports.eu.org/s/clufnf4ri0000ahstpq70gczz" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
+    Your browser doesn't support iframes
+</iframe>
+</section>
+</div>
+
+  )
+}
+
+export default status
