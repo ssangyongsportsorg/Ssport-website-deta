aa049d46956da4f290b2fbd4f1c8f1ad77be2bd1
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-01 21:47:37 +0800

    Update status.tsx

--- pages/feedback/status.tsx
+++ pages/feedback/status.tsx
@@ -5,7 +5,7 @@ function chat() {
         <div>
  <section className="bg-white dark:bg-gray-900">
   <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-
+<Head>
         <title>報告故障</title>
         <meta name="description" content="報告錯誤" />
       </Head>      
