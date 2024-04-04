b654a5555345cd0964dcdd48ad6b2658277be363
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-05 00:28:14 +0800

    Update [tag].js

--- pages/blog/tags/[tag].js
+++ pages/blog/tags/[tag].js
@@ -17,7 +17,7 @@ export default function TagPage({ posts, tag }) {
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:mx-0">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              標記為 "{tag}" 的文章
+              標記為 {tag} 的文章
             </h2>
           </div>
           <div>
