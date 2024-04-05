2c59dd898553bd84fc478ba3ec6bad4b27eb2b3d
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-05 17:33:12 +0800

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,25 +1,47 @@
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
-
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+import LineProvider from "next-auth/providers/line"
+import DiscordProvider from "next-auth/providers/discord"
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    GoogleProvider({
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+   GoogleProvider({
       clientId: process.env.Google_ID,
       clientSecret: process.env.Google_SECRET,
-    }),
-  ],
-  cookies: {
-    pkceCodeVerifier: {
-      name: "next-auth.pkce.code_verifier",
-      options: {
-        httpOnly: true,
-        sameSite: "none",
-        path: "/",
-        secure: true,
-      },
-    },
+    }),  
+     LineProvider({
+      clientId: process.env.Line_ID,
+      clientSecret: process.env.Line_SECRET,
+    }),  
+  DiscordProvider({
+      clientId: process.env.Discord_ID,
+      clientSecret: process.env.Discord_SECRET,
+    }),  ],
+  theme: {
+  logo: "/logo.png", // Absolute URL to image
+},
+callbacks: {
+  async jwt({ token, account }) {
+    // Persist the OAuth access_token to the token right after signin
+    if (account) {
+      token.accessToken = account.access_token
+    }
+    return token
   },
-};
+  async session({ session, token, user }) {
+    // Send properties to the client, like an access_token from a provider.
+    session.accessToken = token.accessToken
+    return session
+    },
 
-export default NextAuth(authOptions);
+  },
+}
+
+
+
+export default NextAuth(authOptions)
