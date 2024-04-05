3b664bb809a127e49373d9c5e527cfdd53836c6f
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-05 23:13:08 +0800

    Update support.tsx

--- pages/support.tsx
+++ pages/support.tsx
@@ -1,6 +1,7 @@
 import Supportchoice from './components/supportchoice';
+import Head1 from './components/head1';
 import Supporthero from './components/supporthero';
-import C123 from './components/C123';
+import Supportend from './components/supportend;
 import styles from './components/docsearch.module.css';
 import Ticket from './components/ticket';
 import Head from 'next/head'
@@ -15,9 +16,10 @@ const support = () => {
         <meta name="description" content="歡迎來到雙龍體育幫助中心,任何問題立刻聯繫" />
       </Head>      
       <div>
+<Head1 />
         <Supporthero />
         <Supportchoice />
-       <C123 />             
+       <Supportend />             
       </div>
     </>
       );
