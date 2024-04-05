14a8bf86d199d757cf9a1d3ba5c38fda13884244
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-06 00:31:47 +0800

    Update layout.tsx

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -3,24 +3,13 @@ import type { ReactNode } from 'react'
 import { tree } from '../source'
 import { Nav } from "next-docs-ui/nav";
 import 'next-docs-ui/style.css'
+import Head1 from '../pages/components/head1';
 export default function RootDocsLayout({ children }: { children: ReactNode }) {
   return (
  <div>
-  <DocsLayout
-    
-    tree={tree}
-    nav={{
-      component: (
-        <Nav
-          title={<>雙龍體育幫助中心</>}
-          url="/support"
-          items={[{ href: "/", children: "返回官網" }]}
-        />
-      ),
-    }}
-  >
+  <Head1 />
     {children}
-  </DocsLayout>
+  
 </div>
   )
 }
