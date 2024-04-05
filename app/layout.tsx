d4dc4fc42fcb9c2ac2b8d2d9dd092bb2e3a1f0f4
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-06 01:00:52 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import type { ReactNode } from 'react'
 import 'next-docs-ui/style.css'
 import '@/styles/globals.css'
 import Footer from '@/pages/components/footer'
-
+import Head1 from '../pages/components/head1';
 export default function Layout({ children }: { children: ReactNode }) {
   const CrispWithNoSSR = dynamic(
     () => import('./components/crisp')
@@ -15,6 +15,7 @@ export default function Layout({ children }: { children: ReactNode }) {
     <html lang="zh-tw">
     <CrispWithNoSSR />
     <body>
+<Head1 />
 <RootProvider>        {children}</RootProvider>
     </body>
           <Footer />   
