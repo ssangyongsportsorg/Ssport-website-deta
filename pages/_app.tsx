7939d51d93d092054d1362daa15ec30f4ca8f7c4
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-05 21:28:07 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,39 +3,27 @@ import type { AppProps } from 'next/app'
 import Head from './components/head'
 import Footer from './components/footer'
 import { SessionProvider } from 'next-auth/react'
-import Script from 'next/script';
+import Script from 'next/script'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
-  // 检查是否是 RestrictedPage，如果是，则不使用 app.tsx
-  if (Component.name === 'support') {
-    return (
-      <div>
-        <div className="bg-gradient h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
-        <Component {...pageProps} />
-      </div>
-    );
-  }
+  const router = useRouter()
+  const isRestrictedRoute = router.pathname.startsWith('/support')
 
-  
   return (
     <div>
-      
-               <div className="bg-gradient h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
-        <SessionProvider session={session}>
-          
-          <Head />
-          <br />
-                    <br />
-
-          <Component {...pageProps} />
-          <Footer />
-        </SessionProvider>
-    
+      <div className="bg-gradient h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
+      <SessionProvider session={session}>
+        {!isRestrictedRoute && <Head />}
+        <br />
+        <br />
+        <Component {...pageProps} />
+        {!isRestrictedRoute && <Footer />}
+      </SessionProvider>
     </div>
-
-    
-  );
+  )
 }
