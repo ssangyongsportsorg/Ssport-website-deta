f903d9890cc337a2de54ea55a3058ef3370f546f
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-07 12:56:11 +0800

    Create send-welcome.ts

---
+++ lib/emails/send-welcome.ts
@@ -0,0 +1,21 @@
+import { Resend } from "resend";
+import { WelcomeEmail } from "@/components/emails/welcome";
+
+const resend = new Resend(process.env.RESEND_API_KEY!);
+
+export async function sendWelcomeEmail({name, email}: {name: string | null | undefined, email: string | null | undefined}) {
+  const emailTemplate = WelcomeEmail({ name });
+  try {
+    // Send the email using the Resend API
+    await resend.emails.send({
+      from: "Marc from Papermark <marc@papermark.io>",
+      to: email as string,
+      subject: "Welcome to Papermark!",
+      react: emailTemplate,
+    });
+  } catch (error) {
+    // Log any errors and re-throw the error
+    console.log({ error });
+    throw error;
+  }
+}
