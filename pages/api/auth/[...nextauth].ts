1b95645524fa7cbeaa06b74c71dc6c4779a7a3bf
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-07 15:56:43 +0800

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth"
 import Providers from "next-auth/providers"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
+import GoogleProvider from "next-auth/providers/google";
 
 const prisma = new PrismaClient()
 
